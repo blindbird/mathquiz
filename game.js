9b1cac44277577bada53901ba0ab301c74de700f
Mei <maylaures@gmail.com>
2014-07-16 13:19:30 -0700

    Showquiz

--- game.js
+++ game.js
@@ -1,2 +1,19 @@
-alert('hello world');
-console.log('Hello world')
+if(jQuery){
+	$("question1").show();
+}
+
+if(impress){
+	$("#question2").show();
+}
+
+if(atom){
+	$("question3").show();
+}
+
+if(createjs){
+	$("question4").show();
+}
+
+if(me){
+	$("question5").show()
+}
